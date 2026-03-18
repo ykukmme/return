22d7daa4d55be02e1ec56565c07b1aa48dda7546
kkm <rdijk021457@gmail.com>
2026-03-18 23:24:24 +0900

    기초부터 복습

---
+++ app/src/main/java/com/example/kotlin1/Example1.kt
@@ -0,0 +1,18 @@
+package com.example.kotlin1
+
+fun main() {
+    println("hello world")
+    val result = test(10, c= 1)
+    println(result)
+    test2("kim", "km", "1515")
+}
+
+fun test3(a: Int, b: Int) = a+b
+
+fun test2(name: String, nickname: String, id: String) = println(name + nickname + id)
+
+fun test(a: Int, b: Int = 3, c: Int = 5): Int {
+    val b = 3
+    println(a+b+c)
+    return a+b+c
+}
