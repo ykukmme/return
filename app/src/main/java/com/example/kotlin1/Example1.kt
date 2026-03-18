f47ba44ad3820219327173a21a5047e0f6272ec1
kkm <rdijk021457@gmail.com>
2026-03-18 23:29:39 +0900

    기초부터 복습

--- app/src/main/java/com/example/kotlin1/Example1.kt
+++ app/src/main/java/com/example/kotlin1/Example1.kt
@@ -7,10 +7,7 @@ fun main() {
     test2("kim", "km", "1515")
 }
 
-fun times(a: Int, b: Int): Int{
-    return a * b
-}
-
+fun times(a: Int, b: Int) = a * b
 fun test3(a: Int, b: Int) = a+b
 
 fun test2(name: String, nickname: String, id: String) = println(name + nickname + id)
